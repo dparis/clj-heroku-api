f6a8efca8d7722aff2cd02e8afeffc8053435e59
Dylan Paris <dylan.paris+github@gmail.com>
2015-09-22 11:03:29 -0700

    Bump version number and remove commented code

--- project.clj
+++ project.clj
@@ -1,16 +1,11 @@
-(defproject clj-heroku-api "0.1.0-SNAPSHOT"
+(defproject clj-heroku-api "0.1.0"
   :description "An always-up-to-date Heroku API client for Clojure"
   :url "http://github.com/dparis/clj-heroku-api"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-
                  [camel-snake-kebab "0.3.2"]
-
-                 ;; Override version 2.3.1 pulled in by Cheshire
-                 ;[com.fasterxml.jackson.core/jackson-core "2.3.2"]
-
                  [cheshire "5.5.0"]
                  [clj-http "2.0.0"]
                  [prismatic/schema "1.0.1"]
