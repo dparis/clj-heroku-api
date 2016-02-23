9e8b6df3772c0015c614ca943b749374547ff115
Dylan Paris <dylan.paris+github@gmail.com>
2016-02-23 10:41:20 -0800

    Update project dependencies

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [camel-snake-kebab "0.3.2"]
                  [cheshire "5.5.0"]
-                 [clj-http "2.0.0"]
-                 [prismatic/schema "1.0.1"]
-                 [com.taoensso/timbre "4.1.1"]]
+                 [clj-http "2.1.0"]
+                 [prismatic/schema "1.0.5"]
+                 [com.taoensso/timbre "4.2.1"]]
 
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies   [[midje "1.7.0"]
-                                    [clj-http-fake "1.0.1"]]
+                   :dependencies   [[midje "1.8.3"]
+                                    [clj-http-fake "1.0.2"]]
                    :plugins        [[lein-midje "3.1.3"]
                                     [codox "0.8.10"]]}}
 
